a3a92248db1020e1fa4994f15855c402c3ed389a
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 19:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someNumber){
+  return Math.abs(42-someNumber)
+}
+
+function distanceFromHqInFeet(someNumber){
+  return 264*distanceFromHqInBlocks(someNumber);
+}
+
+function distanceTravelledInFeet(someNumber){
+
+}
+
+
+function calculatesFarePrice() {
+
+}
