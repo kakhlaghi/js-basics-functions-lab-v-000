cede70e66841dff00eaa3973392fbcd3986a79fc
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 19:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function calculatesFarePrice(startBlock, stopBlock) {
     return 0
   } else if (dist > 400 && dist <= 2000){
     return 0.02*(dist-400)
-  } else if (dist > 2000) {
+  } else if (dist > 2000 && dist <=2500) {
     return 25
   } else {
     return 'cannot travel that far'
